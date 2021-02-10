7fec7095ae2da3b212047914cdc0096bd762d8c2
Austin Cochran <cochran7004@gmail.com>
2021-02-09 21:06:30 -0500

    env added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 8080;
 
 // require dotenv package
 // require('dotenv').config();
-
+// hey
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
